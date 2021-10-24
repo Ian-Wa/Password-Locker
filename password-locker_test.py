21e0e8095c8596120542ec68bebc91247792b16a
moringa <ianwanjira4@gmail.com>
2021-10-24 23:08:47 +0300

    create a test to save multiple credentials object to the credentials_list

--- password-locker_test.py
+++ password-locker_test.py
@@ -66,6 +66,14 @@ class TestCredentials(unittest.TestCase):
         self.new_credential.save_credentials()
         self.assertEqual(len(Credentials.credentials_list),1)
 
+    def test_save_multiple_credentials(self):
+        '''
+        test to check if multiple credentials objects can be saved into the credentials_list.
+        '''
+        self.new_credential.save_credentials()
+        test_credentials = Credentials('Twitter', 'Logan', '6789')
+        test_credentials.save_credentials()
+        self.assertEqual(len(Credentials.credentials_list), 2)
 
 if __name__ == '__main__':
     unittest.main()
